3955a9ee0c6cb39798f84fff193c0d9632d871d5
zendranm <zendran.m@gmail.com>
2022-07-23 17:29:05 +0200

    Adding Translations collection

--- server/main.js
+++ server/main.js
@@ -1,5 +1,34 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
+import { Translations } from "../imports/api/TranslationsCollection";
+import { i18n } from "meteor/universe:i18n";
+import "../imports/locales/en.i18n.yml";
+import "../imports/locales/es.i18n.yml";
+
+const en = {
+  en: {
+    apple: "apple",
+    dog: "dog",
+  },
+};
+
+const es = {
+  es: {
+    apple: "manzana",
+    dog: "perro",
+  },
+};
+
+const insertTranslation = (translations) => {
+  Translations.insert({
+    _id: Object.keys(translations)[0],
+    translations: Object.values(translations)[0],
+    updatedAt: new Date(),
+  });
+};
 
 Meteor.startup(() => {
   // code to run on server at startup
+  if (Translations.find().count() === 0) {
+    [en, es].forEach(insertTranslation);
+  }
 });
